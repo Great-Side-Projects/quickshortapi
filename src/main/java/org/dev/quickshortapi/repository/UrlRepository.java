b429f0693f6440695f85523e2be1e26928f8d837
Angel Morales <angelmoralesb@gmail.com>
2024-03-22 23:13:21 -0500

    Refactorizar metodos de repositorio

--- src/main/java/org/dev/quickshortapi/repository/UrlRepository.java
+++ src/main/java/org/dev/quickshortapi/repository/UrlRepository.java
@@ -1,19 +1,18 @@
 package org.dev.quickshortapi.repository;
 
 import org.dev.quickshortapi.repository.data.Url;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 
 @Repository
 public interface UrlRepository extends MongoRepository<Url, String> {
-    Url findByUrlCorta(String urlCorta);
-    Url findByUrlOriginal(String urlOriginal);
+    Optional<Url> findByUrlCorta(String urlCorta);
+    Optional<Url> findByUrlOriginal(String urlOriginal);
     void deleteByUrlCorta(String urlCorta);
+
+
 
 
 
