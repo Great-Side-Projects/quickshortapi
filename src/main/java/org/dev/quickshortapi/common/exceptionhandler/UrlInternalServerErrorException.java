2aafb953c7a0656074169cf6058ec40d79baf443
Angel Morales <angelmoralesb@gmail.com>
2024-03-22 23:12:40 -0500

    Agregar UrlInternalServerErrorException

---
+++ src/main/java/org/dev/quickshortapi/common/exceptionhandler/UrlInternalServerErrorException.java
@@ -0,0 +1,11 @@
+package org.dev.quickshortapi.common.exceptionhandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class UrlInternalServerErrorException extends RuntimeException {
+    public UrlInternalServerErrorException(String message) {
+        super(message);
+    }
+}
