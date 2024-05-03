39ed1262d93c9200e0d1081c16f00fb41472cce4
Angel Morales <angelmoralesb@gmail.com>
2024-05-03 15:34:31 -0500

    Sonarqube Issues

--- src/main/java/org/dev/quickshortapi/common/shortener/UrlShortener.java
+++ src/main/java/org/dev/quickshortapi/common/shortener/UrlShortener.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.Random;
 
 @Component
 public class UrlShortener implements IUrlShortener {
