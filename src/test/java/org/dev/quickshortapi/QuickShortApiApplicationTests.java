9988a6710e6e5ba2741b736cab528fd2f58b7859
Angel Morales <angelmoralesb@gmail.com>
2024-05-03 14:56:20 -0500

    Sonarqube Issues

--- src/test/java/org/dev/quickshortapi/QuickShortApiApplicationTests.java
+++ src/test/java/org/dev/quickshortapi/QuickShortApiApplicationTests.java
@@ -1,5 +1,6 @@
 package org.dev.quickshortapi;
 
+import org.dev.quickshortapi.common.shortener.IUrlShortener;
 import org.dev.quickshortapi.infraestructure.adapter.in.web.UrlController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,19 @@ class QuickShortApiApplicationTests {
 
     @Autowired
     private UrlController urlController;
+
+    @Autowired
+    private IUrlShortener urlShortener;
     @Test
     void contextLoads() {
         assertThat(urlController).isNotNull();
 
+    }
+    @Test
+    void generateRandomShortUrlEveryTime() {
+        String shortUrl1 = urlShortener.generateRandomShortUrl();
+        String shortUrl2 = urlShortener.generateRandomShortUrl();
+        assertThat(shortUrl1).isNotEqualTo(shortUrl2);
     }
 
 }
