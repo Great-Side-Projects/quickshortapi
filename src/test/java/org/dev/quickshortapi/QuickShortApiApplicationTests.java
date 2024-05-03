e24b07a17efa868aa8261252bc312f72e63ded1b
Angel Morales <angelmoralesb@gmail.com>
2024-05-03 14:24:57 -0500

    Sonarqube Issues

--- src/test/java/org/dev/quickshortapi/QuickShortApiApplicationTests.java
+++ src/test/java/org/dev/quickshortapi/QuickShortApiApplicationTests.java
@@ -1,13 +1,18 @@
 package org.dev.quickshortapi;
 
+import org.dev.quickshortapi.infraestructure.adapter.in.web.UrlController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class QuickShortApiApplicationTests {
 
+    @Autowired
+    private UrlController urlController;
     @Test
     void contextLoads() {
+        assert urlController != null;
     }
 
 }
