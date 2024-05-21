1574c23ed2d53b770cfd461d6bda6896196c7fda
Angel Morales <angelmoralesb@gmail.com>
2024-05-20 20:40:40 -0500

    Circuit breaker cache resilience implementation

--- src/main/java/org/dev/quickshortapi/application/port/out/IUrlPersistenceCachePort.java
+++ src/main/java/org/dev/quickshortapi/application/port/out/IUrlPersistenceCachePort.java
@@ -9,5 +9,8 @@ public interface IUrlPersistenceCachePort {
     UrlCache save(UrlCache urlCache);
     Optional<UrlCache> findById(String id);
     void deleteById(String id);
+    UrlCache fallbackSave(UrlCache urlCache, Throwable t);
+    Optional<UrlCache> fallbackFindById(String id, Throwable t);
+    void fallbackDeleteById(String id, Throwable t);
 
 }
--- src/main/java/org/dev/quickshortapi/infraestructure/adapter/out/persistence/UrlPersistenceCacheAdapter.java
+++ src/main/java/org/dev/quickshortapi/infraestructure/adapter/out/persistence/UrlPersistenceCacheAdapter.java
@@ -1,32 +1,59 @@
 package org.dev.quickshortapi.infraestructure.adapter.out.persistence;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.dev.quickshortapi.application.port.out.IUrlPersistenceCachePort;
 import org.dev.quickshortapi.common.PersistenceAdapter;
 import org.dev.quickshortapi.application.port.out.IUrlRepositoryCache;
+import org.slf4j.ILoggerFactory;
+import org.springframework.data.redis.RedisConnectionFailureException;
 
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @PersistenceAdapter
 public class UrlPersistenceCacheAdapter implements IUrlPersistenceCachePort {
 
     private final IUrlRepositoryCache UrlRepositoryCache;
+    Logger logger = Logger.getLogger(getClass().getName());
 
     public UrlPersistenceCacheAdapter(IUrlRepositoryCache urlRepositoryCache) {
         this.UrlRepositoryCache = urlRepositoryCache;
     }
-   @Override
+
+    @Override
+    @CircuitBreaker(name = "urlPersistenceCache", fallbackMethod = "fallbackSave")
     public UrlCache save(UrlCache urlCache) {
       return UrlRepositoryCache.save(urlCache);
 
     }
 
     @Override
+    @CircuitBreaker(name = "urlPersistenceCache", fallbackMethod = "fallbackFindById")
     public Optional<UrlCache> findById(String id) {
+        //throw new RedisConnectionFailureException("Forced error for testing Circuit Breaker");
         return UrlRepositoryCache.findById(id);
     }
 
     @Override
+    @CircuitBreaker(name = "urlPersistenceCache", fallbackMethod = "fallbackDeleteById")
     public void deleteById(String id) {
         UrlRepositoryCache.deleteById(id);
+    }
+
+    public UrlCache fallbackSave(UrlCache urlCache, Throwable t) {
+        // Implementar lógica de fallback aquí
+        logger.info("Fallback method for save");
+        return null; // o un valor por defecto
+    }
+
+    public Optional<UrlCache> fallbackFindById(String id, Throwable t) {
+        // Implementar lógica de fallback aquí
+        logger.info("Fallback method for findById");
+        return Optional.empty(); // o un valor por defecto
+    }
+
+    public void fallbackDeleteById(String id, Throwable t) {
+        // Implementar lógica de fallback aquí
+        logger.info("Fallback method for deleteById");
     }
 }
