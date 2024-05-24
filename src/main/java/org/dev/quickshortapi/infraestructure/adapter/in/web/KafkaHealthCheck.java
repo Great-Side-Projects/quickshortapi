78dd324c663eeea33a6b5e6e250039494bf7468e
Angel Morales <angelmoralesb@gmail.com>
2024-05-24 14:15:55 -0500

    ping urlVisitedEvent

--- src/main/java/org/dev/quickshortapi/infraestructure/adapter/in/web/KafkaHealthCheck.java
+++ src/main/java/org/dev/quickshortapi/infraestructure/adapter/in/web/KafkaHealthCheck.java
@@ -1,10 +1,15 @@
 package org.dev.quickshortapi.infraestructure.adapter.in.web;
 
+import org.dev.quickshortapi.domain.event.UrlEvent;
+import org.dev.quickshortapi.domain.event.UrlVisitedEvent;
+import org.dev.quickshortapi.infraestructure.adapter.out.persistence.UrlMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -24,8 +29,10 @@ public class KafkaHealthCheck implements HealthIndicator {
     @Override
     public Health health() {
         try {
-            KafkaTemplate.send(test_topic, "❥").get(1, TimeUnit.SECONDS);
-            // catch interrupted exception and return down
+            // ping message to kafka
+            UrlEvent urlEvent = new UrlEvent("1", "http://ping.com", "ping", new Date());
+            UrlVisitedEvent urlVisitedEvent = UrlMapper.toUrlVisitedEvent(urlEvent);
+            KafkaTemplate.send(test_topic, urlVisitedEvent).get(1, TimeUnit.SECONDS);
         } catch (InterruptedException | ExecutionException | TimeoutException | NullPointerException e) {
           return Health.down(e).build();
         }
