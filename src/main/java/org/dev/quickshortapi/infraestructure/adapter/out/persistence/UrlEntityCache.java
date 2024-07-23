4638dcfa16f56de94ab21b470e631ecd54677cd1
Angel Morales <angelmoralesb@gmail.com>
2024-07-23 17:08:16 -0500

    refactoring cache pattern and expiration

--- src/main/java/org/dev/quickshortapi/infraestructure/adapter/out/persistence/UrlEntityCache.java
+++ src/main/java/org/dev/quickshortapi/infraestructure/adapter/out/persistence/UrlEntityCache.java
@@ -3,7 +3,9 @@ package org.dev.quickshortapi.infraestructure.adapter.out.persistence;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
 import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
 
 @RedisHash("UrlCache")
 @Getter
@@ -13,6 +15,8 @@ public class UrlEntityCache implements Serializable {
     private String id;
     private String idDbUrl;
     private String originalUrl;
+    @TimeToLive(unit = TimeUnit.MINUTES)
+    private long expirationTime;
 
     public UrlEntityCache() {
     }
